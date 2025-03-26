2bae57aba3252bb66a7e42633208162c2c3bd387
Ruslan-mac <macbook@Ruslan-mac.local>
2025-03-26 18:28:28 +0400

    davam edir 26 mart saat 18:28

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    slides[slideIndex - 1].style.display = "block";
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].classList.remove("active");
+    }
+    dots[slideIndex - 1].classList.add("active");
+    setTimeout(showSlides, 4000); // 2 saniyədən bir dəyişir
+  }
+
+  function currentSlide(n) {
+    slideIndex = n - 1;
+    showSlides();
+  }
